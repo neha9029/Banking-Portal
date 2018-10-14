bfa1ae04e3365d89324dd82251c8ccce6cc46541
neha9029 <nehanaik9029@gmail.com>
2018-10-14 14:45:41 -0400

    Initialized using Spring Boot starter project

---
+++ src/main/java/com/user/OnlinePortalUserEndApplication.java
@@ -0,0 +1,12 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePortalUserEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePortalUserEndApplication.class, args);
+	}
+}
